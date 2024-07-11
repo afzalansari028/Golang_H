bcc809ecdb9852e138ce035dbe31514d5f6c14f9
afzalansari028 <52788347+afzalansari028@users.noreply.github.com>
2024-07-11 13:24:08 +0530

    Update main.go

--- 19defer/main.go
+++ 19defer/main.go
@@ -9,10 +9,28 @@ func main() {
 	fmt.Println("Hello")
 
 	// myDefer()
+	ValidateAge(12)
 }
 
 func myDefer() {
 	for i := 0; i < 5; i++ {
 		defer fmt.Print(i)
 	}
+}
+
+func ValidateAge(age int) {
+	fmt.Println("hii")
+	defer func() {
+		r := recover()
+		if r != nil {
+			fmt.Println("Recovered panic:", r)
+		}
+	}()
+
+	if age < 18 {
+		panic("You are below 18, you can't vote...")
+	} else {
+		fmt.Println("You are eligible for vote")
+	}
+	fmt.Println("byeee")
 }
