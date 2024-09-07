9572af3e41d55aac5f46cb0ce460d4c8f25b187e
Afzal Ansari <sakshichalpe51@gmail.com>
2024-09-07 22:33:55 +0530

    Revert "Update main.go"
    This reverts commit fd72d2b4e16a0cc07697cfb2b6f36f23187b8004.

--- 10mymaps/main.go
+++ 10mymaps/main.go
@@ -25,33 +25,4 @@ func main() {
 		fmt.Printf("For key %v and for value %v\n", key, value)
 	}
 
-        arr := []int{1, 4, 7, 5, 5, 1, 4, 2, 9, 3}
-	RemoveDuplicate(arr)
-	FrequencyOfElemnts(arr)
-}
-
-func FrequencyOfElemnts(arr []int) {
-	result := make(map[int]int)
-
-	for _, val := range arr {
-		result[val]++
-	}
-	fmt.Println("result---", result)
-
-}
-
-func RemoveDuplicate(arr []int) {
-
-	visited := make(map[int]bool)
-	var result []int
-
-	for _, val := range arr {
-
-		if !visited[val] {
-			visited[val] = true
-
-			result = append(result, val)
-		}
-	}
-	fmt.Println("remove duplicates:::", result)
 }
